0ba01785848890db95018378799c651d528ab6bc
Pooja Mantri <pooja.mantri@infibeam.net>
2017-05-21 09:53:33 +0530

    Added routing for building Single page applications aka SPA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
+import { LocationStrategy, HashLocationStrategy } from '@angular/common'
+
 import {
-FormsModule,
-ReactiveFormsModule
+  FormsModule,
+  ReactiveFormsModule
 } from '@angular/forms';
+
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CreateUserComponent } from './create-user/create-user.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: CreateUserComponent },
+  { path: 'create-ticket', component: CreateUserComponent },
+  { path: 'users', component: CreateUserComponent }]
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateUserComponent
-  ],
+ ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
