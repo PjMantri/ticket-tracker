6539abf9753b17a13e00e02c4f8d3ec0cac7f12e
Pooja Mantri <pooja.mantri@infibeam.net>
2017-05-25 08:33:11 +0530

    Create ticket template created

--- src/app/create-ticket/create-ticket.component.ts
+++ src/app/create-ticket/create-ticket.component.ts
@@ -1,13 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl
+} from '@angular/forms';
+
+import { Http, Response } from '@angular/http';
+import { CreateUserService } from "app/create-user/create-user.service";
+import { User } from "app/user.module";
+import { UsersService } from "app/users/users.service";
+
+
 @Component({
   selector: 'app-create-ticket',
   templateUrl: './create-ticket.component.html',
   styleUrls: ['./create-ticket.component.css']
 })
 export class CreateTicketComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  myForm: FormGroup;
+  status: AbstractControl;
+  area: AbstractControl;
+  description: AbstractControl;
+  customer: AbstractControl;
+  createdBy: AbstractControl;
+  assignedTo: AbstractControl;
+
+  constructor(fb: FormBuilder, private http1: Http, private usersService: UsersService) {
+    this.myForm = fb.group({
+      'name': [''],
+      'email': [''],
+      'type': ['']
+    });
+    this.status = this.myForm.controls['status'];
+    this.area = this.myForm.controls['area'];
+    this.description = this.myForm.controls['description'];
+
+    this.customer = this.myForm.controls['customer'];
+    this.createdBy = this.myForm.controls['createdBy'];
+    this.assignedTo = this.myForm.controls['assignedTo'];
+
+    this.users = usersService.users;
+  }
+
 
   ngOnInit() {
   }
--- src/app/create-ticket/create-ticket.service.ts
+++ src/app/create-ticket/create-ticket.service.ts
@@ -1,5 +1,7 @@
 
 import { User } from "app/user.module";
+import { Ticket } from "app/ticket.module";
+
 import {
     HttpModule,
     Http,
@@ -12,33 +14,38 @@ import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class CreateUserService {
+export class CreateUserTicket {
     http: Http;
     constructor(private http1: Http) {
         this.http = http1;
     }
 
-    makePost(user: User): void {
+    makePost(ticket: Ticket): void {
         console.log("Request body " + JSON.stringify({
-            name: user.name,
-            email: user.email,
-            type: user.type
+            status: ticket.status,
+            area: ticket.area,
+            description: ticket.description,
+            customer:ticket.customer,
+            createdBy:ticket.createdBy,
+            assignedTo:ticket.assignedTo
         }));
 
 
-        let url = 'http://localhost:8080/user/';
+        let url = 'http://localhost:8080/ticket/';
 
         let body = JSON.stringify({
-            name: user.name,
-            email: user.email,
-            type: user.type
+            status: ticket.status,
+            area: ticket.area,
+            description: ticket.description,
+            customer:ticket.customer,
+            createdBy:ticket.createdBy,
+            assignedTo:ticket.assignedTo
         });
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers });
 
-
         this.http.post(url, body, options).subscribe((res: Response) => {
-            console.log("Respose for create user " + res);
+            console.log("Respose for create ticket " + res);
         });
     }
 }
